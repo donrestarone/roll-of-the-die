5f41837899a2d83b8c0fe24a220ad4b90d794fcf
donrestarone <shashikejayatunge@gmail.com>
2018-03-22 16:19:39 -0400

    finish lowest and highest

---
+++ exercises/lowest_and_highest.rb
@@ -0,0 +1,17 @@
+roll_results =[]
+puts "how many times would you like to roll the die?"
+	roll_times = 5
+
+	roll_times.times do |number|
+
+		roll_result = rand(1..6)
+		roll_results << roll_result
+		puts "the result of the roll is #{roll_result}"
+		 
+	end 
+	
+
+sorted_array = roll_results.sort
+
+puts "the highest number is #{sorted_array.last}"
+puts "the lowest number is #{sorted_array.first}"
